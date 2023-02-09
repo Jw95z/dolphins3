a1a3808da849e3251033a8f03a0e4197572ec442
Jw95z <michaelmichael6909@gmail.com>
2023-02-08 21:21:41 -0800

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -48,8 +48,9 @@ if __name__ == "__main__":
     app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
-
-    # db.init_app(app) 
-    # db.app = app 
-    # db.create_all() 
     app.run(host='127.0.0.1', debug=True, port=5000)
+
+
+# db.init_app(app) 
+# db.app = app 
+# db.create_all() 
